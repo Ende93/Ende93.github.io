1a05ae12eba9cb001cd8d86f4837e4eaaa80fdc4
ende93 <ende93@hotmail.com>
2015-11-19 18:15:22 +0800

    update
    change background img url, do js format

--- js/index.js
+++ js/index.js
@@ -1,23 +1,15 @@
 window.onload = function init() {
+
   var snake = [{
       'left': 0,
-      'top': 0,
-      'div': document.getElementById("snakehead")
+      'top' : 0,
+      'div' : document.getElementById("snakehead")
     }],
-    cubesize = 20,
-    timer,
-    delay = 200,
+    cubesize = 20,    
+    delay    = 200,
     maxspeed = 50,
     increasespeed = 1,
-    randombox,
-    box = document.getElementById("box"),
-    lastkeydown,
-    container = {
-      top: box.offsetTop + document.body.scrollTop,
-      left: box.offsetLeft,
-      bottom: box.offsetTop + document.body.scrollTop + 600,
-      right: box.offsetLeft + 600
-    };
+    randombox, timer, lastkeydown;
 
   window.onkeydown = function(e) {
     var event = e || window.event;
@@ -52,13 +44,15 @@ window.onload = function init() {
       case 82: //r
         clearInterval(timer);
 
+        // remove snake body in document
         Array.prototype.slice.call(
             document.getElementsByClassName("snake"))
           .forEach(function(e, i) {
             if (e.id !== "snakehead")
               e.remove();
           });
-
+        
+        // remove snake body in array
         snake.splice(0, snake.length - 1);
 
         snake[0].left = 0;
@@ -73,19 +67,22 @@ window.onload = function init() {
   function move(direction, size, keyCode) {
     return timer = setInterval(function() {
       var head = snake.length - 1;
+
       if (document.getElementsByClassName("randombox").length === 0) {
-        createRandomDiv(Math.abs(size), box);
+        createRandomDiv(Math.abs(size), document.getElementById("box"));
       }
+
       if (size < 0 && snake[head][direction] + size < 0) {
         clearInterval(timer);
         return false;
       } else if (size > 0 && snake[head][direction] + size > 580) {
         clearInterval(timer);
-
         return false;
       }
 
-      if (snake[head].div.style.left == randombox.style.left && snake[head].div.style.top == randombox.style.top) {
+      if (snake[head].div.style.left == randombox.style.left 
+      	&& snake[head].div.style.top == randombox.style.top) {
+
         randombox.className = "snake";
         snake.unshift({
           div: randombox
@@ -95,12 +92,14 @@ window.onload = function init() {
         if(delay > maxspeed)
           delay -= increasespeed;
       }
+
       for (var i = 0; i < head; i++) {
         snake[i].div.style.left = snake[i + 1].div.style.left;
-        snake[i].div.style.top = snake[i + 1].div.style.top;
+        snake[i].div.style.top  = snake[i + 1].div.style.top;
       }
       snake[head][direction] += size;
       snake[head].div.style[direction] = snake[head][direction] + 'px';
+
       for (i = 0; i < head; i++) {
         if (head > 1 && snake[head].div.style.left === snake[i].div.style.left 
         	&& snake[head].div.style.top === snake[i].div.style.top) {
@@ -116,12 +115,44 @@ window.onload = function init() {
   }
 
   function createRandomDiv(size, parent) {
+  	var len  = snake.length,
+  	  i = 0, style, outbody = true;
+
     randombox = document.createElement("div");
     randombox.className = "snake randombox";
 
-    randombox.style.left = size * parseInt(Math.random() * 29, 10) + 'px';
-    randombox.style.top = size * parseInt(Math.random() * 29, 10) + 'px';
+    try {
+      while (len < 30 && outbody) {
+        style = randomStyle(size);
+
+        outbody = snake.some(function(e) {
+          if (e.div.style.left == style.left 
+          	&& e.div.style.top == style.top) {
+            return true;
+          } else {
+            return false;
+          }
+        });
+      }
+    } catch (e) {
+      console.log(e);
+    }
+
+    randombox.style.left = style.left;
+    randombox.style.top  = style.top;
 
     parent.appendChild(randombox);
+
+    function randomStyle(size) {
+      var left = size * Math.floor(Math.random() * 29) + 'px';
+      var top = size * Math.floor(Math.random() * 29) + 'px';
+
+      return {
+      	left : left,
+      	top : top
+      };
+    }
+      
   }
+  
 };
