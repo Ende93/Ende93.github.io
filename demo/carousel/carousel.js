59a7f66305dbf97d7e46b4ffea17877955ed9ba8
ende93 <ende93@hotmail.com>
2017-06-03 23:16:26 +0800

    optimization, but still have .css method need change to pure dom ops

--- demo/carousel/carousel.js
+++ demo/carousel/carousel.js
@@ -1,49 +1,39 @@
 (function () {
 
-  var _carousel = carousel(), _mouse = {},
+  var _carousel = carousel(),
+    _mouse = {},
     TIME = 4000;
 
-  _timer = setInterval(_carousel, TIME);
+  var _html = `<section class="ads-box">
+    <ul class="ads-container"></ul>
+    <ul class="ads-icon-container"></ul>
+  </section>
+  `
+  var _width = 0;
+  var _timer = setInterval(_carousel, TIME);
+  var _$box = null,
+    _$view = null,
+    _$icon = null;
 
-  $('.ads-box').on('touchstart', function (e) {
-    _mouse.start = e.touches[0];
-  });
+  var $ = function (selector) {
+    return document.querySelector(selector);
+  }
 
-  $('.ads-box').on('touchmove', function (e) {
-    _mouse.end = e.touches[e.touches.length - 1]
-  });
+  function mount(ele, srcs) {
+    var div = document.createElement('div');
+    var iconHTML = '', t = [];
 
-  $('.ads-box').on('touchend', function (e) {
-    var t = _mouse.end.clientX - _mouse.start.clientX;
-    if (t > 30) {
-      _carousel(-1);
-    } else if (t < -30) {
-      _carousel(1);
-    } else {
-      return;
+    div.innerHTML = _html;
+
+    ele.appendChild(div.children[0]);
+    for(var i in srcs) {
+      t.push('<li><img src="' + srcs[i] + '"></li>');
     }
+    t.push( t[0] );
+    t.unshift( t.slice(-2, -1) );
+    ele.querySelector('.ads-container').innerHTML = t.join('');
 
-    clearInterval(_timer);
-    _timer = setInterval(_carousel, TIME);
-  });
-
-  function carousel() {
-    var $iconBox,
-      $box = $('.footer .ads-container'),
-      length = $box.find('.ads-item').length;
-
-    if (length == 0) {
-      return function () {
-        clearInterval(_timer);
-      };
-    }
-
-    var _index = 0,
-      $items = $box.find('.ads-item'),
-      boxWidth,
-      _width = $items.eq(0).css('width').replace('px', '');
-    var iconHTML = '';
-    var prop = -_width;
+    _width = $items.eq(0).css('width').replace('px', '');
 
     $box.css({
       'width': (boxWidth = _width * (length + 2) + 'px'),
@@ -61,8 +51,65 @@
     $items.eq(0).clone().appendTo($items.parent());
     $items.eq(0).before($items.eq(-1).clone());
 
-    $iconBox = $box.parents('section').find('.ads-icon-container ul')
+    _$icon = $box.parents('section').find('.ads-icon-container ul')
       .html(iconHTML);
+
+
+    return {
+      view: ele.querySelector('.ads-box'),
+      box: ele.querySelector('.ads-container'),
+      icon: ele.querySelector('.ads-icon-container')
+    }
+  }
+
+  function init(ele, pics, time) {
+    var t = mount(ele)
+    _$view = t.view;
+    _$box = t.box;
+    _$icon = t.icon;
+
+    TIME = time || TIME;
+
+    _$view.addEventListener('touchstart', function (e) {
+      _mouse.start = e.touches[0];
+    });
+
+    _$view.addEventListener('touchmove', function (e) {
+      _mouse.end = e.touches[e.touches.length - 1]
+    });
+
+    _$view.addEventListener('touchend', function (e) {
+      var t = _mouse.end.clientX - _mouse.start.clientX;
+      if (t > 30) {
+        _carousel(-1);
+      } else if (t < -30) {
+        _carousel(1);
+      } else {
+        return;
+      }
+
+      clearInterval(_timer);
+      _timer = setInterval(_carousel, TIME);
+    });
+
+  }
+
+  function carousel() {
+    var $iconBox,
+      length = _$box.querySelectorAll('.ads-item').length;
+
+    if (length == 0) {
+      return function () {
+        clearInterval(_timer);
+      };
+    }
+
+    var _index = 0,
+      $items = $box.find('.ads-item'),
+      boxWidth,
+      
+    var iconHTML = '';
+    var prop = -_width;
 
     function move(offset) {
       _index += offset || 1;
@@ -100,11 +147,10 @@
 
       $iconBox.find('.current-icon').removeClass('current-icon');
       $iconBox.find('i').eq(_index).addClass('current-icon');
-
     }
 
     return move;
   }
 
-  window.carousel = carousel;
+  window.carousel = init;
 }());
